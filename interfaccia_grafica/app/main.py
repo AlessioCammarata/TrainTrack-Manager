d28c2483c1c25984782b0211fe79fea8d4a7a964
alexein <alessiocammarata42@gmail.com>
2024-05-16 18:19:11 +0200

    main

--- interfaccia_grafica/app/main.py
+++ interfaccia_grafica/app/main.py
@@ -6,9 +6,30 @@ from app.algorithm import Algorithm
 import os
 import platform
 
+
+#            __  __             _            
+#      o O O|  \/  |  __ _     (_)    _ _    
+#     o     | |\/| | / _` |    | |   | ' \   
+#    TS__[O]|_|__|_| \__,_|   _|_|_  |_||_|  
+#   {======|_|"""""|_|"""""|_|"""""|_|"""""| 
+#  ./o--000'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
+
+
 # @singleton
 class App(tk.Tk):
+    """
+# APP
+
+The app class is useful to create the root page. In these class you define the parameters of the root window.\n
+Inside this class you can call 2 different method
+
+    """
     def __init__(self):
+        """
+        - This is the constructor of the App Class\n 
+          Here are defined the specific of the root window as the icon or the window-size.\n\n
+          Furthermore an instance of algorithm is created because it is necessary to free the serial ports at the end of execution. 
+        """
         super().__init__()
 
         #Specifiche del root - style e icon
@@ -24,15 +45,21 @@ class App(tk.Tk):
         self.algo = Algorithm()
 
     def on_close_root(self):
+        """- This function is useful to **close the root window and to stop the algorithm** if it was started."""
         self.destroy()
         self.algo.stop_algo()
 
     def refresh(self):
+        """- This function is useful to **refresh** the page when a language change occurs."""
         self.title(data.Textlines[1])
 
 
-if __name__ == "__main__":
-    
+def setup():
+    """
+    - During the **setup** function are determined some important depndencies like the current path of the project, the current SO and processor architecture and also the current language \n 
+      which is standard set on italian language. Moreover if you have connected the serial ports of your arduino devices, they are automatically connected and setted into the data cache.\n
+      If you want to modify the latter, just go to the settings and set the configuration according to your needs.
+    """
     #Ottengo il nome del sistema operativo
     sistema_operativo = platform.system()
     if sistema_operativo == "Darwin": sistema_operativo = "macOS"
@@ -51,9 +78,14 @@ if __name__ == "__main__":
 
     #Aggiorno la lingua, con quella standard+.
     utilities.translate()
-
+    # utilities.get_name_arduino("COM7")
+    
     #Aggiorno le porte collegate nel caso in cui ci siano
     print(utilities.set_port_var())
+
+    return True
+
+if __name__ == "__main__" and setup():
 
     app = App()
     gui = GUI_refactored.GUI(app)
